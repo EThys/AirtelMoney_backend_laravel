115f11d24e357f3afc29be9e73a62cd28abd21a6
ethbergMuzola <ethbergmuzola500@gmail.com>
2024-06-21 13:14:38 +0200

    chore:update app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,8 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withMiddleware(function (Middleware $middleware) {
-		 $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+        $middleware->statefulApi();
     })
+    // ->withMiddleware(function (Middleware $middleware) {
+	// 	 $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+    // })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
